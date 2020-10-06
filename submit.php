40c7211fa83d84ff8a6789bd3b7e83eb91e0bfe5
stevenpolvino <spolv1@brockport.edu>
2020-10-06 16:15:51 -0400

    updated

--- submit.php
+++ submit.php
@@ -14,6 +14,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 //Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
+	exit();
 }
 
 echo "Connected successfully. ";
